cf42c4bbd8748020afc6f8e30942e4207f1d2ba4
XySquare <x_y2@outlook.com>
2017-04-07 17:17:44 +0800

    再次尝试修正大小写问题

---
+++ app/src/main/java/com/xyy/game/ai/AIGame.java
@@ -0,0 +1,17 @@
+package com.xyy.game.ai;
+
+import com.xyy.game.ai.Screen.StartUpScreen;
+import com.xyy.game.framework.Screen;
+import com.xyy.game.framework.impl.AndroidGame;
+
+/**
+ * 主Activity入口
+ * Created by ${XYY} on ${2016/5/27}.
+ */
+public class AIGame extends AndroidGame {
+    //Rename File Name
+    @Override
+    public Screen getStartScreen() {
+        return new StartUpScreen(this);
+    }
+}
